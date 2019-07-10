346febb5e7e99b9f790147f13e069c9267888c03
Tomin <tomin@rapyuta-robotics.com>
2019-07-10 21:02:50 +0900

    Simple test_di_do

--- test_di_do.c
+++ test_di_do.c
@@ -1,21 +1,26 @@
+#include "revpi_do.h"
 #include "revpi_di.h"
 
 int main()
 {
 int pin_state;
-	while(1)
-	{
-	pin_state = 0;
-	writeVariableValue("O_1",pin_state);
-	sleep(1);
-	readVariableValue("I_1",&pin_state);
-	sleep(1);
-	printf("Read value %d from pin I_1 \n", pin_state);
-	pin_state = 1;
-	writeVariableValue("O_1",pin_state);
-	sleep(1);
+        while(1)
+        {
+        pin_state = 0;
+        writeVariableValue("O_1",pin_state);
+        sleep(1);
+        printf("Wrote value %d on pin O_1 \n", pin_state);
         readVariableValue("I_1",&pin_state);
         sleep(1);
         printf("Read value %d from pin I_1 \n", pin_state);
-	}
+
+        pin_state = 1;
+        writeVariableValue("O_1",pin_state);
+        sleep(1);
+        printf("Wrote value %d on pin O_1 \n", pin_state);
+        readVariableValue("I_1",&pin_state);
+        sleep(1);
+        printf("Read value %d from pin I_1 \n", pin_state);
+        }
 }
+
