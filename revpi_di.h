ebd3b4582d916e61d85ac9df44b74ba744de9ca9
Tomin <tomin@rapyuta-robotics.com>
2019-07-10 18:59:51 +0900

    changed read and write variable header files

--- revpi_di.h
+++ revpi_di.h
@@ -10,17 +10,73 @@
 #include <stdbool.h>
 #include "piControlIf.h"
 #include "piControl.h"
-void readVariableValue(char *pszVariableName,int *pin_state )
+void readVariableValue(char *pszVariableName, int *pin_state)
 {
-        int rc;
-        SPIVariable sPiVariable;
-        SPIValue sPIValue;
-        uint8_t i8uValue;
-        strncpy(sPiVariable.strVarName, pszVariableName, sizeof(sPiVariable.strVarName));
-        rc = piControlGetVariableInfo(&sPiVariable);
-        sPIValue.i16uAddress = sPiVariable.i16uAddress;
-        sPIValue.i8uBit = sPiVariable.i8uBit;
-        rc = piControlGetBitValue(&sPIValue);
-        *pin_state  = sPIValue.i8uValue;
+		char format = 'd';
+		bool cyclic = true;
+		bool quiet = false;
+		int rc;
+		SPIVariable sPiVariable;
+		SPIValue sPIValue;
+		uint8_t i8uValue;
+		uint16_t i16uValue;
+		uint32_t i32uValue;
+
+		strncpy(sPiVariable.strVarName, pszVariableName, sizeof(sPiVariable.strVarName));
+		rc = piControlGetVariableInfo(&sPiVariable);
+		if (rc < 0) {
+				printf("Cannot find variable '%s'\n", pszVariableName);
+				return;
+		}
+		if (sPiVariable.i16uLength == 1) {
+				sPIValue.i16uAddress = sPiVariable.i16uAddress;
+				sPIValue.i8uBit = sPiVariable.i8uBit;
+
+
+						rc = piControlGetBitValue(&sPIValue);
+						if (rc < 0)
+								printf("Get bit error\n");
+						else {
+					*pin_state  = sPIValue.i8uValue;
+								}
+		} 
+
+		else if (sPiVariable.i16uLength == 8) {
+
+						rc = piControlRead(sPiVariable.i16uAddress, 1, (uint8_t *) & i8uValue);
+						if (rc < 0)
+								printf("Read error\n");
+						else {
+												*pin_state  = i8uValue;
+						}
+						}
+
+		else if (sPiVariable.i16uLength == 16) {
+
+						rc = piControlRead(sPiVariable.i16uAddress, 2, (uint8_t *) & i16uValue);
+						if (rc < 0)
+								printf("Read error\n");
+						else {      *pin_state  = i16uValue;
+								}
+
+		} 
+
+		else if (sPiVariable.i16uLength == 32) {
+						rc = piControlRead(sPiVariable.i16uAddress, 4, (uint8_t *) & i32uValue);
+						if (rc < 0)
+								printf("Read error\n");
+						else {      
+												*pin_state  = i32uValue;
+						}
+		}
+
+
+		 else
+				printf("Could not read variable %s. Internal Error\n", pszVariableName);
 }
+
+
+
+
+
 
--- revpi_do.h
+++ revpi_do.h
@@ -18,10 +18,23 @@ void writeVariableValue(char *pszVariableName, uint32_t i32uValue)
         SPIValue sPIValue;
         uint8_t i8uValue;
         uint16_t i16uValue;
+
         strncpy(sPiVariable.strVarName, pszVariableName, sizeof(sPiVariable.strVarName));
         rc = piControlGetVariableInfo(&sPiVariable);
-        sPIValue.i16uAddress = sPiVariable.i16uAddress;
-        sPIValue.i8uBit = sPiVariable.i8uBit;
-        sPIValue.i8uValue = i32uValue;
-        rc = piControlSetBitValue(&sPIValue);
+        if (sPiVariable.i16uLength == 1) {
+                sPIValue.i16uAddress = sPiVariable.i16uAddress;
+                sPIValue.i8uBit = sPiVariable.i8uBit;
+                sPIValue.i8uValue = i32uValue;
+                rc = piControlSetBitValue(&sPIValue);
+        } else if (sPiVariable.i16uLength == 8) {
+                i8uValue = i32uValue;
+                rc = piControlWrite(sPiVariable.i16uAddress, 1, (uint8_t *) & i8uValue);
+
+        } else if (sPiVariable.i16uLength == 16) {
+                i16uValue = i32uValue;
+                rc = piControlWrite(sPiVariable.i16uAddress, 2, (uint8_t *) & i16uValue);
+
+        } else if (sPiVariable.i16uLength == 32) {
+                rc = piControlWrite(sPiVariable.i16uAddress, 4, (uint8_t *) & i32uValue);
+        }
 }
