ef4cf72ac768ae7f924780b927e24bb582241d78
Tomin <tomin@rapyuta-robotics.com>
2019-07-11 11:03:28 +0900

    aio test for 4 analog inputs

--- test_aio.c
+++ test_aio.c
@@ -6,23 +6,14 @@
 int main()
 {
 int voltage;
-char pin_number[10];
+char input_variable[15];
+//while(1)
+//{
 for (int i=1; i<=4; i++)
 {
-        char input_variable[] = "InputValue_";
-        sprintf(pin_number,"%d",i);
-        strcat(input_variable,pin_number);
+        snprintf(input_variable, 15, "InputValue_%d", i); 
         readVariableValue(input_variable,&voltage);
         printf("\n voltage of %s : %d \n", input_variable, voltage );
 }
+//}
 }
-
-
-
-
-
-
-
-
-
-
