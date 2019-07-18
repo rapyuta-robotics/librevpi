c84638f2e3b427828b576dbde09f96e10b3e884c
Bhuvanchandra DV <bhuvanchandra.dv@rapyuta-robotics.com>
2019-07-18 17:50:40 +0900

    librevpi: initial commit

---
+++ include/revpi.h
@@ -0,0 +1,27 @@
+#ifndef REVPI_H_
+#define REVPI_H_
+
+#include <stdint.h>
+#include "piControl.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct revpi_peripheral_t {
+    int pi_control_fd;
+    char pin_name[32];
+    SDeviceInfo* device_info;
+    SPIValue* value;
+    SPIVariable* variable;
+} revpi_peripheral;
+
+int revpi_init(revpi_peripheral* revpi);
+int revpi_set_do_level(revpi_peripheral* revpi, uint8_t level);
+int revpi_get_di_level(revpi_peripheral* revpi);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* REVPI_H_ */
